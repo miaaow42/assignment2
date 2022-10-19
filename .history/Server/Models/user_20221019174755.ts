6ed70d070ad7830bc029bdd4aa70549f992a5741
Yingying Feng <yingyingfeng@YingyingdeMacBook-Pro.local>
2022-10-19 17:50:17 -0400

    added user module

---
+++ .history/Server/Models/user_20221019174755.ts
@@ -0,0 +1,25 @@
+// Step 1 - import mongoose
+import mongoose from "mongoose";
+const Schema = mongoose.Schema; //alias for mongoose.Schema
+
+// Step 2 - Create a Schema that matches the data
+const UserSchema = new Schema
+({
+    DisplayName: String, // mongoose data types
+    username: String,
+    EmailAddress: String,
+    Created: 
+    {
+        type: Date,
+        default: Date.now();
+    }
+},
+{
+    collection: "album"
+});
+
+// Step 3 - Create a Model using the Schema
+const Model = mongoose.model("User",UserSchema); // declare model
+
+// Step 4 - Export the Model -> this makes the file to a module
+export default Model;
